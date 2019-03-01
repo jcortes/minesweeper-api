0dcdcb8ef72604884389203e2ef4e3691f0bf88c
Jorge Cortes <jcortes@belatrixsf.com>
2019-03-01 17:40:17 -0500

    added new start logic

--- app/server/writeModel/playing/game.js
+++ app/server/writeModel/playing/game.js
@@ -29,33 +29,64 @@ const initialState = {
   }
 };
 
+const getIndex = (cols, x, y) => 
+  (x > cols || x <= 0) || (y > cols || y <= 0)
+    ? -1
+    : cols * (y - 1 ) + x - 1;
+
+const shuffle = arr => {
+  const newArr = [...arr];
+  for (let idx = 0; idx < newArr.length; idx += 1) {
+    const rnd = Math.floor(Math.random() * (idx + 1));
+    [newArr[idx], newArr[rnd]] = [newArr[rnd], newArr[idx]];
+  }
+  return newArr;
+};
+
 const commands = {
   start: [
     only.ifNotExists(),
     (game, command) => {
-      const squares = [
-        { mines: 2, flagged: false, revealed: false },
-          { mines: -1, flagged: false, revealed: false },
-            { mines: 2, flagged: false, revealed: false },
-              { mines: -1, flagged: false, revealed: false },
-        { mines: -1, flagged: false, revealed: false },
-          { mines: 2, flagged: false, revealed: false },
-            { mines: 3, flagged: false, revealed: false },
-              { mines: 2, flagged: false, revealed: false },
-        { mines: 1, flagged: false, revealed: false },
-          { mines: 1, flagged: false, revealed: false },
-            { mines: 1, flagged: false, revealed: false },
-              { mines: -1, flagged: false, revealed: false },
-        { mines: 0, flagged: false, revealed: false },
-          { mines: 0, flagged: false, revealed: false },
-            { mines: 1, flagged: false, revealed: false },
-              { mines: 1, flagged: false, revealed: false }
-      ];
+      const { rows, cols, mines } = command.data;
+      const numOfSquares = cols * rows;
+      const rate = mines / numOfSquares;
+      const mineChance = Math.floor(rate * numOfSquares);
+      const mineSquares = Array(mines).fill(-1);
+      const safeSquares = Array(numOfSquares - mines).fill(0);
+      const squares =
+        shuffle([...mineSquares, ...safeSquares])
+          .map((minesNum, idx, arr) => {
+            const isMine = minesNum < 0;
+            const x = Math.floor((idx + 1) % cols) || cols;
+            const y = Math.ceil((idx + 1) / cols);
+            const top = [x, y - 1];
+            const bottom = [x, y + 1];
+            const topLeft = [x - 1, y - 1];
+            const left = [x - 1, y];
+            const bottomLeft = [x - 1, y + 1];
+            const topRight = [x + 1, y - 1];
+            const right = [x + 1, y];
+            const bottomRight = [x + 1, y + 1];
+            const neighborCords = [
+              top, bottom,
+              topLeft, left, bottomLeft,
+              topRight, right, bottomRight
+            ];
+            const label =
+              !isMine
+                ? neighborCords
+                    .map(([x, y]) => arr[getIndex(cols, x, y)])
+                    .filter(mine => mine)
+                    .length
+                : minesNum;
+            return { mines: label, flagged: false, revealed: false };
+          });
+
       game.events.publish("started", {
         nickname: command.user.token.nickname,
-        numOfRows: command.data.rows,
-        numOfCols: command.data.cols,
-        numOfMines: command.data.mines,
+        numOfRows: rows,
+        numOfCols: cols,
+        numOfMines: mines,
         squares
       });
     }
